cd327ce4e98c9dfce66eed073d597b0ba80f4482
garpoz <behrouz.ashraf@gmail.com>
2022-05-08 02:47:07 +0430

    add html file and css file

--- create_image.py
+++ create_image.py
@@ -2,11 +2,38 @@
 # garpozir@gmail.com
 
 
-import sqlite3
+import sqlite3, random
 
 
 def main() -> None:
-    pass
+    rnd = str(random.randint(1, 10243))
+    con = sqlite3.connect("./jomle.sqlite")
+    cur = con.cursor()
+    cur.execute("SELECT * FROM jomle WHERE Id= '%s'" % rnd)
+    db = cur.fetchall()
+    esm = db[0][1]
+    matn = db[0][2]
+    con.close()
+    html_text = f"""
+<!DOCTYPE html>
+<html dir="rtl" lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>insta_story</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+    <div class="row" id="ck">
+        <img id="img-id" class="row" src="./story_theme.jpg" alt="Error Load Image">
+        <div class="bottom">{esm}</div>
+        <div class="text">{matn}</div>
+    </div>
+</body>
+</html>
+    """
+    with open("./format/index.html", "w") as index:
+        index.write(html_text)
+        index.close()
 
 
 if __name__ == "__main__":
--- create_sqliteDb.py
+++ create_sqliteDb.py
@@ -6,7 +6,7 @@ from time import sleep
 
 
 def main() -> None:
-    connection = sqlite3.connect("jomle.sqlite")
+    connection = sqlite3.connect("./jomle.sqlite")
     cursor = connection.cursor()
     cursor.execute(
         """CREATE TABLE IF NOT EXISTS jomle
@@ -15,15 +15,15 @@ def main() -> None:
     connection.commit()
     connection.close()
     sleep(1)
-    with open("jomle_txt/esm.txt", "r") as esm:
+    with open("./jomle_txt/esm.txt", "r") as esm:
         esm_line = esm.readlines()
         esm.close()
     sleep(1)
-    with open("jomle_txt/matn.txt", "r") as matn:
+    with open("./jomle_txt/matn.txt", "r") as matn:
         matn_line = matn.readlines()
         matn.close()
     sleep(1)
-    con = sqlite3.connect("jomle.sqlite")
+    con = sqlite3.connect("./jomle.sqlite")
     cur = con.cursor()
     for i in range(len(matn_line)):
         cur.execute(
